8d741b9f9ec0c86c9e0336fd31e18af07560782a
Sahil Sharma <8928515874.ss@gmail.com>
2023-01-17 12:18:14 +0530

    lab-7: Added example for SSM Parameter store.
     Files added:
     - main.tf
    
     Modifies:
     - .gitignore

---
+++ lab-7/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "general" {
+  name = "My general security group"
+
+  dynamic "ingress" {
+    for_each = ["80", "8080", "443"]
+    content {
+      description = "Allow HTTPS ports"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name  = "General-SG"
+    Owner = "Sahil Sharma"
+  }
+}
+
+# ------------------------------------------------
+output "my_SG_id" {
+  value = aws_security_group.general.id
+}
